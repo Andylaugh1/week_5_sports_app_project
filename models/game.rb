f9474a18a9c303f0d0ce2d4a59f67991828ed11e
Andy Laughlin <atl11384@gmail.com>
2018-05-05 09:56:17 +0100

    CRUD methods complete

--- models/game.rb
+++ models/game.rb
@@ -21,6 +21,25 @@ class Game
     @id = game_data.first()['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE * FROM games WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM games WHERE id = $1"
+    values = [@id]
+    game_data = SqlRunner.run(sql, values)
+    return Game.new(game_data.first)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM games"
+    game_data = SqlRunner.run(sql)
+    return game_data.map { |game| Game.new(game) }
+  end
+
   def self.delete_all()
     sql = "DELETE FROM games"
     SqlRunner.run(sql)
--- models/team.rb
+++ models/team.rb
@@ -17,6 +17,27 @@ class Team
     @id = team_data.first()['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE * FROM teams WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM teams WHERE id = $1"
+    values = [@id]
+    team_data = SqlRunner.run(sql, values)
+    result = Team.new (team_data.first)
+    return result
+  end
+
+  def self.all()
+    sql = "SELECT * FROM teams"
+    team_data = SqlRunner.run(sql)
+    result = team_data.map { |team| Team.new (team) }
+    return result
+  end
+
   def self.delete_all()
     sql = "DELETE FROM teams"
     SqlRunner.run(sql)
