3295152aa2ae8d45f9ff89b059c91f9e94dd8627
Andy Laughlin <atl11384@gmail.com>
2018-05-07 08:10:48 +0100

    CRUD actions completed for Game views. Initial css applied

--- controllers/game_controller.rb
+++ controllers/game_controller.rb
@@ -3,7 +3,35 @@ require('sinatra/contrib/all')
 require_relative('../models/game.rb')
 require_relative('../models/team.rb')
 
+
+# READ ALL GAMES
 get '/games' do
   @games = Game.all()
   erb( :"game/index" )
+end
+
+# CREATE GAME
+get '/games/new' do
+  @teams = Team.all()
+  erb( :"game/new")
+end
+
+post '/games' do
+  game = Game.new(params)
+  game.save()
+  redirect to("/games")
+end
+
+# SHOW INDIVIDUAL GAME
+
+get '/games/:id' do
+  @game = Game.find(params[:id])
+  erb(:"game/show")
+end
+
+# DELETE GAME
+post '/games/:id/delete' do
+  game = Game.find(params[:id])
+  game.delete
+  redirect to ("/games")
 end
